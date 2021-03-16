717b1bbb3e090a212fd3f64bd46926a96e91e21a
carokartu <carokrstulovich@gmail.com>
2021-03-16 09:46:51 -0300

    agrego js y scss

--- index.js
+++ index.js
@@ -4,18 +4,51 @@ const $$ = document.querySelectorAll //  selector ALL
 let comics = [] // guarda data de comics  API
 let characters = [] // guarda data de personajes API 
 
-
-const API_KEY = ".."
-const BASE_URL ="__"
+const API_KEY = "b1ee9360739b9c7554ec7be096d4d06f"
+const URLMARVEL ="https://gateway.marvel.com/v1/public"
 
 let offset = 0 
 let page = 0 
 let lastsearch = ""
+let ComicsInPage = 20
 
 const form = $(".form-search");
 const mainSeccion = $(".main-section");
 const resultsTitle = $(".results-title-container"); // div contenedor 
 const resultsCards = $(".results-cards-container");
-const resultsNumberTitle = $(".results-found")
-const loader = $(".loader-container")
+const resultsNumberTitle = $(".results-found");
+const loader = $(".loader-container");
 
+const buttonSearch = $(".search-button");
+const pageButton = $$ (".page-button") ; 
+
+const previusPageButton = $(".previous-page") ;
+const nextPageButton = $(".next-page");
+const lastPageButton = $(".last-page") ;
+const firstPageButton = $(".first-page") ;
+
+
+
+
+/**  RUTAS */
+const getComics = `${URLMARVEL}/comics?apikey=${API_KEY}`;
+const getCharacters = `${URLMARVEL}/characters?apikey=${API_KEY}`;
+const getCharactersNextPage = `${getCharacters}&offset=${page *ComicsInPage}&orderBy=name`
+
+
+
+/**  FUNCIONES */
+
+
+
+const deleteHTML = (element) => {
+    element.innerHTML = ``;
+  }
+  
+  const hidde = (element) => {
+    element.classList.add("is-hidden")
+  }
+  
+  const show = (element) => {
+    element.classList.remove("is-hidden")
+  }
